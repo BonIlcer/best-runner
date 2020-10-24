6f6de11a08a43363f361c5e0b219c4dc01d856d7
BonIlcer <pantaev.e@mail.ru>
2020-10-24 18:41:40 +0700

    Edit styles WorkoutItem

--- src/components/WorkoutItem.js
+++ src/components/WorkoutItem.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function WorkoutItem({ item }) {
   return (
-    <div className="workout-item">
-      <h3>Workout Item</h3>
+    <div className="workout-item" style={{ borderBottom: "1px solid white" }}>
+      <h4>Workout Item</h4>
       <p>{JSON.stringify(item)}</p>
     </div>
   );
